266dd989570929513d685b5fb9a7cbfe35bb9225
Cody Dietrich <cody.dietrich156@gmail.com>
2017-10-27 23:40:42 -0500

    Views/urls configured for new pages

--- ISHARonline/ISHAR/urls.py
+++ ISHARonline/ISHAR/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^/IntegratedHealth', views.int_health, name='int_health')
+    url(r'^IntegratedHealth/$', views.int_health, name='int_health'),
+    #url(r'^/IntegratedCulture/$', views.int_culture, name='int_culture'),
+    #url(r'^/JournalPortal/$', views.journal_portal, name='journal'),
+    url(r'^Cosmology/$', views.cosmology, name='cosmology'),
+    url(r'^Contact/$', views.contact, name='contact'),
+
 ]
--- ISHARonline/ISHAR/views.py
+++ ISHARonline/ISHAR/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Article, Category
+
 def index(request):
 	return render(request, 'ISHAR/index.html', {})
+
+def int_health(request):
+	#category_list = Category.objects
+	return render(request, 'ISHAR/intHealth.html', {})
+
+def cosmology(request):
+	return render(request, 'ISHAR/compWorldCosmologies.html', {})
+
+def contact(request):
+	return render(request, 'ISHAR/contact.html', {})
