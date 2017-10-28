c3596c1b56be831d0cda5b662f646261912fcde8
Cody Dietrich <cody.dietrich156@gmail.com>
2017-10-28 04:49:04 -0500

    Updated admin

--- ISHARonline/ISHAR/admin.py
+++ ISHARonline/ISHAR/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Article, Category
+from .models import Article, Category, SubCategory
 
 admin.site.register(Article)
 admin.site.register(Category)
+admin.site.register(SubCategory)
