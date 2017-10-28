047fd5c66b9d2df32c17eb492e0f53eb46533d9a
Cody Dietrich <cody.dietrich156@gmail.com>
2017-10-27 23:50:56 -0500

    Linked pages together

--- ISHARonline/ISHAR/urls.py
+++ ISHARonline/ISHAR/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'ishar'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^IntegratedHealth/$', views.int_health, name='int_health'),
