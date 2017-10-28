5326d83861997d8697e06202180993874658d1c5
Cody Dietrich <cody.dietrich156@gmail.com>
2017-10-27 21:43:12 -0500

    Fixed setting and html templates

--- ISHARonline/ISHARonline/settings.py
+++ ISHARonline/ISHARonline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ISHAR.apps.IsharConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
