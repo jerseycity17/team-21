a1c0031eafb10a868f6a868716aa6d07c648634f
Cody Dietrich <cody.dietrich156@gmail.com>
2017-10-28 02:09:45 -0500

    Most site views/urls configured. Links working.

--- ISHARonline/ISHAR/urls.py
+++ ISHARonline/ISHAR/urls.py
@@ -6,9 +6,17 @@ app_name = 'ishar'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^IntegratedHealth/$', views.int_health, name='int_health'),
-    #url(r'^/IntegratedCulture/$', views.int_culture, name='int_culture'),
-    #url(r'^/JournalPortal/$', views.journal_portal, name='journal'),
+    url(r'^IntegratedCulture/$', views.int_culture, name='int_culture'),
+    url(r'^JournalPortal/$', views.journal_portal, name='journal'),
     url(r'^Cosmology/$', views.cosmology, name='cosmology'),
     url(r'^Contact/$', views.contact, name='contact'),
+    url(r'^Affilliates/$', views.affilliates, name='affilliates'),
+    url(r'^Article/(?P<article_id>[0-9]+)/$', views.article_desc, name='article_desc'),
+    url(r'^(?P<category>[a-z]+)/Articles/$', views.article_list, name='article_list'),
+    url(r'^(?P<category>[a-z]+)/$', views.category, name='category'),
+    url(r'^Mission/$', views.mission, name='mission'),
+    url(r'^SupervisingBoard/$', views.super_board, name='super_board'),
+    url(r'^bmhJournal/$', views.bmh_journal, name='bmh_journal'),
+
 
 ]
--- ISHARonline/ISHAR/views.py
+++ ISHARonline/ISHAR/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Article, Category
@@ -10,8 +10,40 @@ def int_health(request):
 	#category_list = Category.objects
 	return render(request, 'ISHAR/intHealth.html', {})
 
+def int_culture(request):
+	return render(request, 'ISHAR/intCulture.html', {})
+
 def cosmology(request):
 	return render(request, 'ISHAR/compWorldCosmologies.html', {})
 
 def contact(request):
 	return render(request, 'ISHAR/contact.html', {})
+
+def affilliates(request):
+	return render(request, 'ISHAR/affilliates.html', {})
+
+def article_desc(request, article_id):
+	article = get_object_or_404(Article, pk=article_id)
+	return render(request, 'ISHAR/article_desc.html', {'article' : article})
+
+def article_list(request, category):
+	article_list = get_list_or_404(Article, category=category)[:10]
+	return render(request, 'ISHAR/articleList.html', {'article_list'})
+
+def category(request, category):
+	return render(request, 'ISHAR/categoryDesc.html', {'category : category'})
+
+def mission(request):
+	return render(request, 'ISHAR/mission.html', {})
+
+def super_board(request):
+	return render(request, 'ISHAR/supervisingBoard.html', {})
+
+def journal_portal(request):
+	return render(request, 'ISHAR/journalPortal.html', {})
+
+def bmh_journal(request):
+	return render(request, 'ISHAR/bmhJournal.html', {})
+
+
+
